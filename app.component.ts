49a5152fee014693638646b75ae8adcc928054de
jcherian007 <53785199+jcherian007@users.noreply.github.com>
2019-08-05 10:15:54 -0400

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+    templateUrl:'app/app.component.html?V=1.0'
+})
+export class AppComponent { name: String = 'Employee Details' }
+
+//import { Component } from '@angular/core';
+//@Component({
+//    selector:'my-app',
+//    template:`<h1>Hello {{name }}</h1>`
+//})
+
+//export class AppComponent {
+//    name : string = "John Cherian3"
+//}
